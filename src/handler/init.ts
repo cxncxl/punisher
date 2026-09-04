d29f4b040262a2ec840bab0371dd4ad62e34b7ad
cxncxl <danyil.mahalias@icloud.com>
2026-09-04 23:24:50 +0300

    fix: allow `undefined` in Firestore writes

--- src/handler/init.ts
+++ src/handler/init.ts
@@ -1,7 +1,10 @@
 import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
 /**
  * Global Firebase Admin initialization.
  * Must be executed before any Firebase services (Firestore, etc.) are accessed.
  */
 initializeApp();
+
+getFirestore().settings({ ignoreUndefinedProperties: true });
